c7fe3485ae8f30b3dbe6888a5ad1faa35d27e8b5
Henrik Rydgård <hrydgard@gmail.com>
2021-03-14 00:16:44 +0100

    Some progress towards making the file browser work with DOCUMENT_TREE

--- Common/File/DirListing.h
+++ Common/File/DirListing.h
@@ -30,11 +30,14 @@ struct FileInfo {
 bool GetFileInfo(const char *path, FileInfo *fileInfo);
 
 enum {
-	GETFILES_GETHIDDEN = 1
+	GETFILES_GETHIDDEN = 1,
+	GETFILES_URIENCODE = 2,  // Android shenanigans
 };
 size_t GetFilesInDir(const char *directory, std::vector<FileInfo> *files, const char *filter = nullptr, int flags = 0);
+
+// rootDir should NOT include the directory separator.
 #ifndef _WIN32
-size_t GetFilesInDirByFd(int fd, std::vector<FileInfo> *files, const char *filter = nullptr, int flags = 0);
+size_t GetFilesInDirByFd(int fd, const std::string &rootDir, std::vector<FileInfo> *files, const char *filter = nullptr, int flags = 0);
 #endif
 int64_t GetDirectoryRecursiveSize(const std::string &path, const char *filter = nullptr, int flags = 0);
 
