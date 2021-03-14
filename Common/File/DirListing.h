854e388165c8b88ecfd33f9c09321c8e7248df42
Henrik Rydgård <hrydgard@gmail.com>
2021-03-14 10:53:40 +0100

    Remove the defunct fdopendir approach. Buildfixes.

--- Common/File/DirListing.h
+++ Common/File/DirListing.h
@@ -33,12 +33,8 @@ enum {
 	GETFILES_GETHIDDEN = 1,
 	GETFILES_URIENCODE_ANDROID = 2,  // Android shenanigans
 };
-size_t GetFilesInDir(const char *directory, std::vector<FileInfo> *files, const char *filter = nullptr, int flags = 0);
 
-// rootDir should NOT include the directory separator.
-#ifndef _WIN32
-size_t GetFilesInDirByFd(int fd, const std::string &rootDir, std::vector<FileInfo> *files, const char *filter = nullptr, int flags = 0);
-#endif
+size_t GetFilesInDir(const char *directory, std::vector<FileInfo> *files, const char *filter = nullptr, int flags = 0);
 int64_t GetDirectoryRecursiveSize(const std::string &path, const char *filter = nullptr, int flags = 0);
 
 #ifdef _WIN32
